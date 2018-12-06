aecfa8c00b873ad956a12eb7e91fc7950e014506
sávio <saviopf22@gmail.com>
2018-12-06 04:45:23 -0300

    segunda questão feita em python mas falta fazer em c e a terceira questao

---
+++ questao2py.py
@@ -0,0 +1,10 @@
+tamanho = int(input('tamanho do vetor~> '))
+
+vetor = []
+
+for i in range(tamanho):
+    vetor.append(int(input('elemento para o vetor~> ')))
+
+vetor = list(set(vetor))
+
+print(vetor)
